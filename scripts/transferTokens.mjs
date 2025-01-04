b3adf17b234ad80a7af8c22df09d047b8c2c76d3
Dima <dimakislitsyn1996@gmail.com>
2025-01-04 14:43:24 +0200

    Worked on UI, needs logic resolved

--- scripts/transferTokens.mjs
+++ scripts/transferTokens.mjs
@@ -1,14 +1,15 @@
-import { ethers } from "ethers"; 
+import { ethers } from "ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
-const AMOUNT = ethers.parseUnits("1.0", 18); 
+const AMOUNT = ethers.parseUnits("1.0", 18);
 
-async function transferTokens() {
+
+export default async function transferTokens() {
     const provider = new ethers.JsonRpcProvider(process.env.HOLESKY_RPC_URL);
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-   
+
     const ABI = [
         "function transfer(address to, uint256 amount) public returns (bool)"
     ];
@@ -16,12 +17,12 @@ async function transferTokens() {
 
     const tokenContract = new ethers.Contract(process.env.SEPOLIA_CONTRACT_ADDRESS, ABI, wallet);
 
-    
+
     const tx = await tokenContract.transfer(process.env.WALLET_ADDRESS, AMOUNT);
     console.log("Transaction Hash:", tx.hash);
 
     const receipt = await tx.wait();
     console.log("Transaction Confirmed:", receipt);
+
 }
 
-transferTokens().catch(console.error);
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { ethers } from "ethers";
 import faucetContract from "../../ethereum/faucet.js";
-import { Web3Provider } from '@ethersproject/providers'; 
-
+import { Web3Provider } from '@ethersproject/providers';
 
 // import dotenv from "dotenv";
 // dotenv.config();
+
+const AMOUNT = ethers.parseUnits("1.0", 18);
+
+const HOLESKY_RPC_URL = "https://holesky.infura.io/v3/dbcc98661c9c43d88e57ccb78128e417"
+const PRIVATE_KEY = "1ad3614ba54e329f040a9d61de11f8969adaa7d8e9973ae1e8ca7be0d7c27e96"
+const SEPOLIA_CONTRACT_ADDRESS = "0x8e39fc83d3cF574E2207fC6fE18c65DdDaF8D77f"
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
@@ -15,6 +20,28 @@ function App() {
   const [withdrawError, setWithdrawError] = useState("");
   const [withdrawSuccess, setWithdrawSuccess] = useState("");
   const [transactionData, setTransactionData] = useState("");
+
+
+  async function transferTokens(walletAddress) {
+    const provider = new ethers.JsonRpcProvider(HOLESKY_RPC_URL);
+    const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+
+
+    const ABI = [
+        "function transfer(address to, uint256 amount) public returns (bool)"
+    ];
+
+
+    const tokenContract = new ethers.Contract(SEPOLIA_CONTRACT_ADDRESS, ABI, wallet);
+
+
+    const tx = await tokenContract.transfer(walletAddress, AMOUNT);
+    console.log("Transaction Hash:", tx.hash);
+
+    const receipt = await tx.wait();
+    console.log("Transaction Confirmed:", receipt);
+
+}
 
   useEffect(() => {
     getCurrentWalletConnected();
@@ -54,7 +81,7 @@ function App() {
       try {
         /* get provider */
         console.log("provider:");
-        const provider = new Web3Provider(window.ethereum); 
+        const provider = new Web3Provider(window.ethereum);
         /* get accounts */
         const accounts = await provider.send("eth_accounts", []);
         if (accounts.length > 0) {
@@ -89,104 +116,86 @@ function App() {
   };
 
   const getOCTHandler = async () => {
-    setWithdrawError("");
-    setWithdrawSuccess("");
-    const provider = new ethers.providers.JsonRpcProvider(process.env.HOLESKY_RPC_URL);
-    console.log("url:", process.env.HOLESKY_RPC_URL);
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    const ABI = [
-      "function transfer(address to, uint256 amount) public returns (bool)"
-    ];
-
-
-    // const tokenContract = new ethers.Contract(process.env.SEPOLIA_CONTRACT_ADDRESS, ABI, wallet);
-    //To
-    const tokenContract = new ethers.Contract("0xf7C34b29fC625bEB1E9EbA543e6Cb91b43Aa9172", ABI, wallet);
-
-
-    const tx = await tokenContract.transfer(process.env.WALLET_ADDRESS, 1000);
-    //From
-
-
-
-    console.log("Transaction Hash:", tx.hash);
-
-    const receipt = await tx.wait();
-    console.log("Transaction Confirmed:", receipt);
+    try {
+      const tx = await transferTokens(walletAddress);
+      setTransactionData(tx.hash);
+    } catch (err) {
+      console.error(err.message);
+      setWithdrawError(err.message);
+    }
 
   };
 
   return (<div>
     <div className="backgroundDiv">    </div>
 
-      <section className="hero ">
-        <div className="faucet-hero-body">
-          <div className="container has-text-centered main-content">
-            <h1 className="title is-1">Faucet</h1>
-            <p>Fast and reliable. 50 OCT/day.</p>
-            <div className="mt-5">
-              {withdrawError && (
-                <div className="withdraw-error">{!withdrawError ? <div style={{ color: "green" }}>Successfully sent</div>
-                  :
-                  <div style={{ color: "red" }}>User rejected {withdrawError}</div>}
-                </div>
-              )}
-              {withdrawSuccess && (
-                <div className="withdraw-success">{withdrawSuccess}</div>
-              )}{" "}
+    <section className="hero ">
+      <div className="faucet-hero-body">
+        <div className="container">
+          <h1 className="title is-1">Send Tokens</h1>
+          <p>Fast and reliable.</p>
+          {withdrawError && (
+            <div className="withdraw-error">{!withdrawError ? <div style={{ color: "green" }}>Successfully sent</div>
+              :
+              <div style={{ color: "red" }}>User rejected {withdrawError}</div>}
             </div>
-            {walletAddress &&
+          )}
+          {withdrawSuccess && (
+            <div className="withdraw-success">{withdrawSuccess}</div>
+          )}{" "}
+          <div className="connectedButtonDiv">
+            <button className="connectedButton" onClick={connectWallet}>
+              <span className="is-link has-text-weight-bold">
+                {walletAddress && walletAddress.length > 0
+                  ? `Connected: ${walletAddress.substring(
+                    0,
+                    6
+                  )}...${walletAddress.substring(38)}`
+                  : "Connect Wallet"}
+              </span>
+            </button>
+          </div>
+          {walletAddress &&
 
-              <div className="box address-box">
+            <div>
 
-                <div className="connectedButtonDiv">
-                  <button className="connectedButton" onClick={connectWallet}>
-                    <span className="is-link has-text-weight-bold">
-                      {walletAddress && walletAddress.length > 0
-                        ? `Connected: ${walletAddress.substring(
-                          0,
-                          6
-                        )}...${walletAddress.substring(38)}`
-                        : "Connect Wallet"}
-                    </span>
+
+              <div className="columns">
+                <div className="column is-four-fifths">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    placeholder="Enter your wallet address (0x...)"
+                    defaultValue={walletAddress}
+                  />
+                </div>
+                <div className="column">
+                  <button
+                    className="button is-link is-medium"
+                    onClick={getOCTHandler}
+                    disabled={walletAddress ? false : true}
+                  >
+                    Send OCT
                   </button>
                 </div>
-                <div className="columns">
-                  <div className="column is-four-fifths">
-                    <input
-                      className="input is-medium"
-                      type="text"
-                      placeholder="Enter your wallet address (0x...)"
-                      defaultValue={walletAddress}
-                    />
-                  </div>
-                  <div className="column">
-                    <button
-                      className="button is-link is-medium"
-                      onClick={getOCTHandler}
-                      disabled={walletAddress ? false : true}
-                    >
-                      Send OCT
-                    </button>
-                  </div>
-                </div>
-                <article className="panel is-grey-darker">
-                  <p className="panel-heading">Transaction Data</p>
-                  <div className="panel-block">
-                    <p>
-                      {transactionData
-                        ? `Transaction hash: ${transactionData}`
-                        : "--"}
-                    </p>
-                  </div>
-                </article>
               </div>
-            }
+              <article className="panel is-grey-darker">
+                <p className="panel-heading">Transaction Data</p>
+                <div className="panel-block">
+                  <p style={{display: "flex", justifyContent: "center"}}>
+                    {transactionData
+                      ? `Transaction hash: ${transactionData}`
+                      : "--"}
+                  </p>
+                </div>
+              </article>
+            </div>
+          }
 
-          </div>
         </div>
-      </section>
-    </div>);
+      </div>
+    </section>
+  </div>);
 }
 
 export default App;
